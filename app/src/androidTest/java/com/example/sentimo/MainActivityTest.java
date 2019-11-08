c7e93574f0e7a0ec334349a37bee4af79fdc6a06
Sam Sun <sunscssc@gmail.com>
2019-11-08 03:01:47 -0700

    Implement SortMood test

--- app/src/androidTest/java/com/example/sentimo/MainActivityTest.java
+++ app/src/androidTest/java/com/example/sentimo/MainActivityTest.java
@@ -1,6 +1,5 @@
 package com.example.sentimo;
 
-import android.app.Activity;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -11,10 +10,14 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
-import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import java.util.Date;
+import java.util.Random;
 
+import static org.junit.Assert.*;
+
+/**
+ * Test class for main activity
+ */
 public class MainActivityTest {
 
     private Solo solo;
@@ -22,7 +25,6 @@ public class MainActivityTest {
     @Rule
     public ActivityTestRule<MainActivity> rule =
             new ActivityTestRule<>(MainActivity.class, true, true);
-
 
     /**
      * Runs before all tests and creates solo instance.
@@ -34,19 +36,8 @@ public class MainActivityTest {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
     }
 
-    /**
-     * Gets the Activity
-     *
-     * @throws Exception
-     */
-    @Test
-    public void start() throws Exception {
-        Activity activity = rule.getActivity();
-    }
 
-    @Test
-    public void addMood() {
-        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+    private void removeOldMoods() {
         solo.waitForView(R.id.mood_list);
         // delete all existing moods for testing
         ListView moodList = (ListView) solo.getView(R.id.mood_list);
@@ -57,7 +48,15 @@ public class MainActivityTest {
             solo.waitForView(R.id.mood_list);
         }
         assertEquals(moodList.getAdapter().getCount(), 0);
+    }
 
+    /**
+     * Test if adding a mood works
+     */
+    @Test
+    public void addMood() {
+        solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
+        removeOldMoods();
         solo.clickOnButton("Add");
         solo.clickOnButton("Add Emotion");
         // click on happy button
@@ -75,20 +74,15 @@ public class MainActivityTest {
 
     }
 
+    /**
+     * Test if editing a mood works
+     */
     @Test
     public void editMood() {
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-        // delete all existing moods for testing
-        ListView moodList = (ListView) solo.getView(R.id.mood_list);
-        int moodCount = moodList.getAdapter().getCount();
-        for (int i = 0; i < moodCount; ++i) {
-            solo.clickLongInList(0);
-            solo.clickOnButton("YES");
-            solo.waitForView(R.id.mood_list);
-        }
-        assertEquals(moodList.getAdapter().getCount(), 0);
 
         // add a mood to edit
+        removeOldMoods();
         solo.clickOnButton("Add");
         solo.clickOnButton("Add Emotion");
         // click on sad button
@@ -114,19 +108,13 @@ public class MainActivityTest {
         assertTrue(solo.waitForText("Somewhat sad", 1, 2000));
     }
 
+    /**
+     * Test if deleting a mood works
+     */
     @Test
     public void deleteMood() {
         solo.assertCurrentActivity("Wrong Activity", MainActivity.class);
-        solo.waitForView(R.id.mood_list);
-        // delete all existing moods for testing
-        ListView moodList = (ListView) solo.getView(R.id.mood_list);
-        int moodCount = moodList.getAdapter().getCount();
-        for (int i = 0; i < moodCount; ++i) {
-            solo.clickLongInList(0);
-            solo.clickOnButton("YES");
-            solo.waitForView(R.id.mood_list);
-        }
-        assertEquals(moodList.getAdapter().getCount(), 0);
+        removeOldMoods();
 
         solo.clickOnButton("Add");
         solo.clickOnButton("Add Emotion");
@@ -148,8 +136,67 @@ public class MainActivityTest {
 
         assertFalse(solo.waitForText(date, 1, 2000));
         assertFalse(solo.waitForText(time, 1, 2000));
-
     }
 
+    /**
+     * Test if moods are sorted reverse chronologically
+     */
+    @Test
+    public void sortMood() {
+        removeOldMoods();
+        // add 3 moods to sort
+        solo.clickOnButton("Add");
+        solo.clickOnButton("Add Emotion");
+        solo.clickOnView(solo.getView(R.id.happyButton));
+        solo.enterText((EditText) solo.getView(R.id.reason_editText), "Happy");
+        solo.clickOnButton("Done");
+
+        solo.clickOnButton("Add");
+        solo.clickOnButton("Add Emotion");
+        solo.clickOnView(solo.getView(R.id.sadButton));
+        solo.enterText((EditText) solo.getView(R.id.reason_editText), "Sad");
+        solo.clickOnButton("Done");
+
+        solo.clickOnButton("Add");
+        solo.clickOnButton("Add Emotion");
+        solo.clickOnView(solo.getView(R.id.tiredButton));
+        solo.enterText((EditText) solo.getView(R.id.reason_editText), "Tired");
+        solo.clickOnButton("Done");
+
+        // edit the time of the moods randomly
+        // edit 3 rounds and check final order
+        for (int i = 0; i < 3; ++i) {
+            for (int j = 0; j < 3; ++j) {
+                solo.clickInList(j);
+                solo.clearEditText((EditText) solo.getView(R.id.time_text));
+                solo.clearEditText((EditText) solo.getView(R.id.date_text));
+                Random random = new Random();
+                int day = random.nextInt(25) + 1;
+                int month = random.nextInt(11) + 1;
+                // generate a number between 0 to 12
+                int hour = random.nextInt(12);
+                // generate a number between 0 to 60
+                int minute = random.nextInt(12);
+                solo.enterText((EditText) solo.getView(R.id.time_text), String.format("%d:%d", hour, minute));
+                solo.enterText((EditText) solo.getView(R.id.date_text), String.format("%d/%d/2019", day, month));
+                solo.clickOnButton("Confirm Edit");
+                solo.waitForView(R.id.mood_list);
+            }
+        }
+
+        ListView moodList = (ListView) solo.getView(R.id.mood_list);
+        Date lastDate = null;
+        // check order one by one
+        for (int i = 0; i < 3; ++i) {
+            Mood mood = (Mood) moodList.getAdapter().getItem(i);
+            Date thisDate = mood.getTime().getTime();
+            if (lastDate != null) {
+                // compare to returns a negative number when current date is earlier than last date
+                assertTrue(thisDate.compareTo(lastDate) < 0);
+            }
+            lastDate = thisDate;
+        }
+
+    }
 }
 

39abf1d3d87cc6dbf01de68dc844ea9077d7b1d3
Axirr <sherlock.scott@gmail.com>
2019-11-26 22:10:08 -0700

    Added documentation.

--- app/src/main/java/com/example/sentimo/DatabaseListener.java
+++ app/src/main/java/com/example/sentimo/DatabaseListener.java
@@ -1,5 +1,8 @@
 package com.example.sentimo;
 
+/**
+ * Listener interface to control behaviour after network request return
+ */
 public interface DatabaseListener {
     void onSuccess();
     void onFailure();

7000c148fd0e764ac9b0fed1a051b091724e3959
Axirr <sherlock.scott@gmail.com>
2019-11-26 23:59:14 -0700

    Refactor Situation class. Added SituationType enum.

--- app/src/main/java/com/example/sentimo/Situations/OnePersonSituation.java
+++
@@ -1,7 +0,0 @@
-package com.example.sentimo.Situations;
-
-public class OnePersonSituation extends Situation {
-    public OnePersonSituation() {
-        this.name = "One Person";
-    }
-}
--- app/src/main/java/com/example/sentimo/Situations/Situation.java
+++ app/src/main/java/com/example/sentimo/Situations/Situation.java
@@ -3,6 +3,27 @@ package com.example.sentimo.Situations;
 public class Situation {
     String name;
 
+    public Situation() {}
+
+    public Situation(SituationType situationType) {
+        switch (situationType) {
+            case Alone:
+                this.name = "Alone";
+                break;
+            case OnePerson:
+                this.name = "One Person";
+                break;
+            case SeveralPeople:
+                this.name = "Several People";
+                break;
+            case Crowd:
+                this.name = "Crowd";
+                break;
+            default:
+                throw new RuntimeException("Unknown SituationType");
+        }
+    }
+
     public String getName() {
         return this.name;
     }

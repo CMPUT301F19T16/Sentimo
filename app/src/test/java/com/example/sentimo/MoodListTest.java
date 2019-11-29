15a51c5581e828663eda57f4f923ba741aa1e5be
Devon <dsavrtka@ualberta.ca>
2019-11-28 22:21:24 -0700

    Updated MoodListTest with the new Mood format

--- app/src/test/java/com/example/sentimo/MoodListTest.java
+++ app/src/test/java/com/example/sentimo/MoodListTest.java
@@ -1,71 +1,81 @@
 package com.example.sentimo;
 
+import com.example.sentimo.Emotions.Emotion;
+import com.example.sentimo.Emotions.EmotionType;
+import com.example.sentimo.Situations.Situation;
+import com.example.sentimo.Situations.SituationType;
+
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MoodListTest {
-    @Test
-    public void reworkTests() {
-        // Must rework commented out tests for new Mood class
-        Assert.assertEquals(false, true);
+
+
+    private List<Mood> mockMoodList(){
+        List<Mood> moodList = new ArrayList<>();
+        moodList.add(mockMood());
+        return moodList;
     }
 
-//    private List<Mood> mockMoodList(){
-//        List<Mood> moodList = new ArrayList<>();
-//        moodList.add(mockMood());
-//        return moodList;
-//    }
+    private Mood mockMood(){
+        TimeFormatter timef = new TimeFormatter();
+        try{
+            timef.setTimeFormat("Nov 28, 2019", "7:21 PM");
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
+        return new Mood(timef, new Emotion(EmotionType.Happy), "test reason", new Situation(SituationType.Alone),
+                        53.5, 113.5, null);
+    }
 
-//    private Mood mockMood(){
-//        TimeFormatter timef = new TimeFormatter();
-//        try{
-//            timef.setTimeFormat("01/01/2019", "03:07");
-//        }catch (ParseException e){
-//            e.printStackTrace();
-//        }
-//        return new Mood(timef, new Happy(), "test reason", new AloneSituation(), false);
-//    }
-//
-//    @Test
-//    public void testAddMood(){
-//        List<Mood> moods = mockMoodList();
-//        Assert.assertEquals(1, moods.size());
-//
-//        TimeFormatter timef = new TimeFormatter();
-//        try{
-//            timef.setTimeFormat("01/02/2019", "03:08");
-//        } catch (ParseException e){
-//            e.printStackTrace();
-//        }
-//        Mood mood = new Mood(timef, new Sad(), "", new OnePersonSituation(), true);
-//        moods.add(mood);
-//
-//        Assert.assertEquals(2, moods.size());
-//    }
-//
-//    @Test
-//    public void testRemoveMood(){
-//        List<Mood> moods = mockMoodList();
-//        TimeFormatter timef = new TimeFormatter();
-//        try{
-//            timef.setTimeFormat("02/05/2019", "12:12");
-//        } catch (ParseException e){
-//            e.printStackTrace();
-//        }
-//        Mood mood = new Mood(timef, new Tired(), "blah", new CrowdSituation(), false);
-//        Mood mood2 = new Mood(timef, new Worried(), "", new SeveralPeopleSituation(), true);
-//
-//        moods.add(mood);
-//        moods.add(mood2);
-//
-//        Assert.assertEquals(3, moods.size());
-//
-//        moods.remove(mood);
-//
-//        Assert.assertEquals(2, moods.size());
-//
-//        moods.remove(mood2);
-//
-//        Assert.assertEquals(1, moods.size());
-//    }
+    @Test
+    public void testAddMood(){
+        List<Mood> moods = mockMoodList();
+        Assert.assertEquals(1, moods.size());
+
+        TimeFormatter timef = new TimeFormatter();
+        try{
+            timef.setTimeFormat("Nov 29, 2019", "3:08 AM");
+        } catch (ParseException e){
+            e.printStackTrace();
+        }
+        Mood mood = new Mood(timef, new Emotion(EmotionType.Mad), null,
+                            new Situation(SituationType.SeveralPeople), null,
+                    null, "images/123456/...");
+        moods.add(mood);
+
+        Assert.assertEquals(2, moods.size());
+    }
+
+    @Test
+    public void testRemoveMood(){
+        List<Mood> moods = mockMoodList();
+        TimeFormatter timef = new TimeFormatter();
+        try{
+            timef.setTimeFormat("Feb 26, 2019", "12:12 AM");
+        } catch (ParseException e){
+            e.printStackTrace();
+        }
+        Mood mood = new Mood(timef, new Emotion(EmotionType.Tired), "blah", null,
+                    50.3, 112.5, null);
+        Mood mood2 = new Mood(timef, new Emotion(EmotionType.Worried), null, null,
+                    null, null, null);
+
+        moods.add(mood);
+        moods.add(mood2);
+
+        Assert.assertEquals(3, moods.size());
+
+        moods.remove(mood);
+
+        Assert.assertEquals(2, moods.size());
+
+        moods.remove(mood2);
+
+        Assert.assertEquals(1, moods.size());
+    }
 }
